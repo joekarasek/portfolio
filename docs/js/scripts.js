003ed3bd8758e1009c225c70a734ada1f7c4c101
Joe Karasek <joseph.karasek@gmail.com>
2017-11-27 12:16:47 -0800

    remove docs from repo, no longer needed for github pages

--- docs/js/scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  removeLoader('loader', 'main');
-});
-
-function removeLoader(loaderTarget, mainTarget) {
-  document.getElementById(loaderTarget).style.display = "none";
-}
